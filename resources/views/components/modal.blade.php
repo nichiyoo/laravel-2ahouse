fd9d0f31d3e2c6befdb80ab3cb917e38e58136c4
nichiyoo <61191577+nichiyoo@users.noreply.github.com>
2025-05-08 12:07:35 +0700

    refactor: enhance modal component functionality and improve visibility handling

--- resources/views/components/modal.blade.php
+++ resources/views/components/modal.blade.php
@@ -9,13 +9,11 @@
   ]);
 @endphp
 
-<div x-data="{ show: @js($show) }" x-trap.inert.noscroll="show"
-  x-on:open-modal.window="$event.detail == '{{ $name }}' ? show = true : null"
-  x-on:close-modal.window="$event.detail == '{{ $name }}' ? show = false : null"
+<div x-data="modal" x-trap.inert.noscroll="show" x-on:open-modal.window="open" x-on:close-modal.window="close"
   x-on:keydown.escape.window="show = false" x-show="show"
-  class="fixed inset-0 z-50 grid items-end overflow-y-auto sm:px-0" style="display: {{ $show ? 'block' : 'none' }};">
+  class="fixed inset-0 z-50 grid items-end overflow-y-auto sm:px-0" x-cloak>
 
-  <div x-show="show" class="fixed inset-0 transition-all transform backdrop-blur-sm" x-on:click="show = false"
+  <div x-show="show" class="fixed inset-0 transition-all transform" x-on:click="show = false"
     x-transition:enter="ease-out duration-300" x-transition:enter-start="opacity-0" x-transition:enter-end="opacity-100"
     x-transition:leave="ease-in duration-200" x-transition:leave-start="opacity-100" x-transition:leave-end="opacity-0">
     <div class="absolute inset-0 bg-zinc-950/50"></div>
@@ -25,28 +23,47 @@
     x-transition:enter-start="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
     x-transition:enter-end="opacity-100 translate-y-0 sm:scale-100" x-transition:leave="ease-in duration-200"
     x-transition:leave-start="opacity-100 translate-y-0 sm:scale-100"
-    x-transition:leave-end="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95" @click.outside="show = false">
+    x-transition:leave-end="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95" x-on:click.outside="show = false">
     {{ $slot }}
   </div>
 </div>
 
-<script>
-  document.addEventListener('alpine:init', () => {
-    // Ensure the modal can be triggered via JavaScript
-    window.openModal = (name) => {
-      window.dispatchEvent(
-        new CustomEvent('open-modal', {
-          detail: name
-        })
-      )
-    }
+@once
 
-    window.closeModal = (name) => {
-      window.dispatchEvent(
-        new CustomEvent('close-modal', {
-          detail: name
-        })
-      )
-    }
-  });
-</script>
+  <script>
+    document.addEventListener('alpine:init', () => {
+      Alpine.data('modal', () => ({
+        name: @js($name),
+        show: @js($show),
+
+        open(event) {
+          if (event.detail === this.name) {
+            this.show = true;
+          }
+        },
+
+        close(event) {
+          if (event.detail === this.name) {
+            this.show = false;
+          }
+        }
+      }));
+
+      window.openModal = (name) => {
+        window.dispatchEvent(
+          new CustomEvent('open-modal', {
+            detail: name
+          })
+        )
+      }
+
+      window.closeModal = (name) => {
+        window.dispatchEvent(
+          new CustomEvent('close-modal', {
+            detail: name
+          })
+        )
+      }
+    });
+  </script>
+@endonce
